9c7ca0cef75bbac63fa271c10e3a871ea6be6f25
wff <wff@wff-PC>
2018-04-07 23:30:26 +0800

    tip&format

--- hbase-server/src/main/java/org/apache/hadoop/hbase/monitoring/MonitoredTask.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/monitoring/MonitoredTask.java
@@ -25,54 +25,65 @@ import org.apache.hadoop.hbase.classification.InterfaceAudience;
 
 @InterfaceAudience.Private
 public interface MonitoredTask extends Cloneable {
-  enum State {
-    RUNNING,
-    WAITING,
-    COMPLETE,
-    ABORTED;
-  }
+    enum State {
+        RUNNING, WAITING, COMPLETE, ABORTED;
+    }
 
-  long getStartTime();
-  String getDescription();
-  String getStatus();
-  long getStatusTime();
-  State getState();
-  long getStateTime();
-  long getCompletionTimestamp();
+    long getStartTime();
 
-  void markComplete(String msg);
-  void pause(String msg);
-  void resume(String msg);
-  void abort(String msg);
-  void expireNow();
+    String getDescription();
 
-  void setStatus(String status);
-  void setDescription(String description);
+    String getStatus();
 
-  /**
-   * Explicitly mark this status as able to be cleaned up,
-   * even though it might not be complete.
-   */
-  void cleanup();
+    long getStatusTime();
 
-  /**
-   * Public exposure of Object.clone() in order to allow clients to easily 
-   * capture current state.
-   * @return a copy of the object whose references will not change
-   */
-  MonitoredTask clone();
+    State getState();
 
-  /**
-   * Creates a string map of internal details for extensible exposure of 
-   * monitored tasks.
-   * @return A Map containing information for this task.
-   */
-  Map<String, Object> toMap() throws IOException;
+    long getStateTime();
 
-  /**
-   * Creates a JSON object for parseable exposure of monitored tasks.
-   * @return An encoded JSON object containing information for this task.
-   */
-  String toJSON() throws IOException;
+    long getCompletionTimestamp();
+
+    void markComplete(String msg);
+
+    void pause(String msg);
+
+    void resume(String msg);
+
+    void abort(String msg);
+
+    void expireNow();
+
+    void setStatus(String status);
+
+    void setDescription(String description);
+
+    /**
+     * Explicitly mark this status as able to be cleaned up, even though it might
+     * not be complete.
+     */
+    void cleanup();
+
+    /**
+     * Public exposure of Object.clone() in order to allow clients to easily capture
+     * current state.
+     * 
+     * @return a copy of the object whose references will not change
+     */
+    MonitoredTask clone();
+
+    /**
+     * Creates a string map of internal details for extensible exposure of monitored
+     * tasks.
+     * 
+     * @return A Map containing information for this task.
+     */
+    Map<String, Object> toMap() throws IOException;
+
+    /**
+     * Creates a JSON object for parseable exposure of monitored tasks.
+     * 
+     * @return An encoded JSON object containing information for this task.
+     */
+    String toJSON() throws IOException;
 
 }
